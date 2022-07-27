73fe703398abe4fafbbdc638a9fcc58eedfe4bb6
Nélia Simas <neliasimas@hotmail.com>
2022-07-27 14:35:10 -0300

    Aprendendo front-end
    Aprendendo front-end

---
+++ Site9/app.js
@@ -0,0 +1,17 @@
+//variáveis
+
+var teste1 = 1;
+$nome = "Renan";
+
+console.log(teste1);
+
+teste1 = "Filipe";
+console.log(teste1);
+console.log($nome);
+
+var $nome = 'aoisjaosi';
+var _nome = 'osdi';
+
+console.log($nome);
+console.log(_nome);
+
